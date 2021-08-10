be6fc7f03f5c956334d18aa812ec04c135b075a7
luowei <luowei_jr@163.com>
2021-08-10 22:12:01 +0800

    增加注册页面

---
+++ miniprogram/pages/registration/index.js
@@ -0,0 +1,131 @@
+// miniprogram/pages/registration/index.js
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+    name: '',
+    mobile: '',
+    gender: '',
+    genderId: 1,
+    genderPopupShow: false,
+    genderColumns: ['男', '女'],
+    birthdayPopupShow: false,
+    birthday: '',
+    birthdayMinYear: new Date(1959, 1, 1).getTime(),
+    birthdayCurrDate: new Date(1980, 1, 1).getTime(),
+    formatter(type, value) {
+      if (type === 'year') {
+        return `${value}年`;
+      }
+      if (type === 'month') {
+        return `${value}月`;
+      }
+      return `${value}日`;
+    },
+  },
+
+  /**
+   * 性别输入框点击时,唤起弹出层和性别选择器
+   */
+  onGenderInputClick: function (options) {
+    console.log(this.data.genderId)
+    this.setData({
+      genderPopupShow: true
+    })
+  },
+
+  onGenderConfirm: function (options) {
+    console.log(options)
+    this.setData({
+      gender: options.detail.value,
+      genderId: options.detail.index + 1,
+      genderPopupShow: false,
+    });
+  },
+
+  onGenderCancel: function (options) {
+    this.setData({
+      genderPopupShow: false
+    });
+  },
+
+  /**
+   * 生日输入框点击时,唤起弹出层和生日选择器
+   */
+  onBirthdayInputClick: function (options) {
+    this.setData({
+      birthdayPopupShow: true
+    })
+  },
+
+  onBirthdayConfirm: function (options) {
+    this.setData({
+      birthday: new Date(options.detail).toISOString().substring(0,10),
+      birthdayPopupShow: false,
+    });
+  },
+
+  onBirthdayCancel: function (options) {
+    this.setData({
+      birthdayPopupShow: false,
+    });
+  },
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function () {
+
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function () {
+
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function () {
+
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function () {
+
+  }
+})
