3e311c7601ed3c1b5624faa2431e5f3f48450808
Zhang-zhixiong <1031049799@qq.com>
2021-10-15 23:39:02 +0800

    苍老师等

--- miniprogram/pages/index/index.js
+++ miniprogram/pages/index/index.js
@@ -5,137 +5,43 @@ const { envList } = require('../../envList.js')
 Page({
   data: {
     showUploadTip: false,
-    powerList: [{
-      title: '云函数',
-      tip: '安全、免鉴权运行业务代码',
-      showItem: false,
-      item: [{
-        title: '获取OpenId',
-        page: 'getOpenId'
+    novel:[
+      {
+        name: "苍井空 Sola Aoi",
+        comment: "著名的日本成人模特、AV女优、电视演员。因为其“童颜巨乳”的特色，获得极大的回响与超高人气，并连续在2003年及2004年蝉连日本《VideoBoy》杂志年度AV女优第一名，在东亚甚至欧美都有知名度。",
+        imagePath: "../../images/teacher-1.jpg"
       },
-      //  {
-      //   title: '微信支付'
-      // },
-       {
-        title: '生成小程序码',
-        page: 'getMiniProgramCode'
+      {
+        name: "松岛枫 Kaede Matsushima",
+        comment: "2002年出道，隶属于宇宙企划，实际入行动机据本人访谈中说就是想要车想出国，男友则是她第一部戏的导演。担任男友执导的AV女主角。松岛枫在用“行动”支持男朋友，推出许多AV片。在加入宇宙企划出了4部片，之后在hmp/MissChristine拍了两部片子，后来便于2003年转到了AliceJapan成了专属女优，历经五年AV生涯拍片上百部。",
+        imagePath: "../../images/teacher-2.jpg"
       },
-      // {
-      //   title: '发送订阅消息',
-      // }
+      {
+        name: "小泽玛丽亚 Maria Ozawa",
+        comment: "小泽玛利亚在高中毕业后，开始打工。在听说了朋友的朋友在做AV女优的事情之后，她也决定去做AV女优。2005年10月，出演了S1レーベル公司发行的AV作品后，作为AV女优正式出道。2006年8月26日，出演《チャンス!3海の家の宴》，这是她第一次在演唱会中亮相。",
+        imagePath: "../../images/teacher-3.jpg"
+      },
+      {
+        name: "神谷姬 Hime Kamiya",
+        comment: "神谷姬原本是Million公司旗下艺人，隶属于REALWORKS公司。2005年一月出道的神谷姬，是MILLION相当看好的未来之星。2005年日本dvd冠军，走的是娇小清纯路线。由于她气质出众并且与女主播内田恭子神情相似，因此有“日本2005AV界头号一线新人”之称，在日本被誉为“美神”。",
+        imagePath: "../../images/teacher-4.jpg"
+      },      
+    ],
+    imageUrls: ['../../images/pic-1.jpg', '../../images/pic-2.jpg', '../../images/pic-3.jpg'],
+    teachNav: [
+      {images: '../../images/icon-1.png', text:'亚洲'},
+      {images: '../../images/icon-2.png', text:'日本'},
+      {images: '../../images/icon-3.png', text:'韩国'},
+      {images: '../../images/icon-4.png', text:'欧美'},
+      {images: '../../images/icon-5.png', text:'非洲'},
+      {images: '../../images/icon-6.png', text:'德国'},
+      {images: '../../images/icon-6.png', text:'朝鲜'},
+      {images: '../../images/icon-5.png', text:'印度'},
+      {images: '../../images/icon-4.png', text:'越南'},
+      {images: '../../images/icon-3.png', text:'美国'},
+      {images: '../../images/icon-2.png', text:'英国'},
+      {images: '../../images/icon-1.png', text:'罗斯'}
     ]
-    }, {
-      title: '数据库',
-      tip: '安全稳定的文档型数据库',
-      showItem: false,
-      item: [{
-        title: '创建集合',
-        page: 'createCollection'
-      }, {
-        title: '更新记录',
-        page: 'updateRecord'
-      }, {
-        title: '查询记录',
-        page: 'selectRecord'
-      }, {
-        title: '聚合操作',
-        page: 'sumRecord'
-      }]
-    }, {
-      title: '云存储',
-      tip: '自带CDN加速文件存储',
-      showItem: false,
-      item: [{
-        title: '上传文件',
-        page: 'uploadFile'
-      }]
-    }, {
-      title: '云托管',
-      tip: '不限语言的全托管容器服务',
-      showItem: false,
-      item: [{
-        title: '部署服务',
-        page: 'deployService'
-      }]
-    }],
-    envList,
-    selectedEnv: {"envId":"test1-7geyrua9048724ef","alias":"test1"},
-    haveCreateCollection: false
   },
-
-  onClickPowerInfo(e) {
-    const index = e.currentTarget.dataset.index
-    const powerList = this.data.powerList
-    powerList[index].showItem = !powerList[index].showItem
-    if (powerList[index].title === '数据库' && !this.data.haveCreateCollection) {
-      this.onClickDatabase(powerList)
-    } else {
-      this.setData({
-        powerList
-      })
-    }
-  },
-
-  onChangeShowEnvChoose() {
-    wx.showActionSheet({
-      itemList: this.data.envList.map(i => i.alias),
-      success: (res) => {
-        this.onChangeSelectedEnv(res.tapIndex)
-      },
-      fail (res) {
-        console.log(res.errMsg)
-      }
-    })
-  },
-
-  onChangeSelectedEnv(index) {
-    // if (this.data.selectedEnv.envId === this.data.envList[index].envId) {
-    //   return
-    // }
-    const powerList = this.data.powerList
-    powerList.forEach(i => {
-      i.showItem = false
-    })
-    this.setData({
-      powerList,
-      haveCreateCollection: false
-    })
-  },
-
-  jumpPage(e) {
-    wx.navigateTo({
-      url: `/pages/${e.currentTarget.dataset.page}/index?envId=${this.data.selectedEnv.envId}`,
-    })
-  },
-
-  onClickDatabase(powerList) {
-    wx.showLoading({
-      title: '',
-    })
-    wx.cloud.callFunction({
-      name: 'quickstartFunctions',
-      config: {
-        env: this.data.selectedEnv.envId
-      },
-      data: {
-        type: 'createCollection'
-      }
-    }).then((resp) => {
-      if (resp.result.success) {
-        this.setData({
-          haveCreateCollection: true
-        })
-      }
-      this.setData({
-        powerList
-      })
-      wx.hideLoading()
-    }).catch((e) => {
-      console.log(e)
-      this.setData({
-        showUploadTip: true
-      })
-      wx.hideLoading()
-    })
-  }
+  
 })
