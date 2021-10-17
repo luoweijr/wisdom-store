eab0c4f4b583d3becd3a27f31c581586bfd0dc0d
Zhang-zhixiong <1031049799@qq.com>
2021-10-17 21:54:06 +0800

    pic修改

--- miniprogram/pages/index/index.js
+++ miniprogram/pages/index/index.js
@@ -8,18 +8,18 @@ Page({
     tutors:[],
     imageUrls: ['../../images/pic-1.jpg', '../../images/pic-2.jpg', '../../images/pic-3.jpg'],
     teachNav: [
-      {images: '../../images/icon-1.png', text:'亚洲'},
-      {images: '../../images/icon-2.png', text:'日本'},
-      {images: '../../images/icon-3.png', text:'韩国'},
-      {images: '../../images/icon-4.png', text:'欧美'},
-      {images: '../../images/icon-5.png', text:'非洲'},
-      {images: '../../images/icon-6.png', text:'德国'},
-      {images: '../../images/icon-6.png', text:'朝鲜'},
-      {images: '../../images/icon-5.png', text:'印度'},
-      {images: '../../images/icon-4.png', text:'越南'},
-      {images: '../../images/icon-3.png', text:'美国'},
-      {images: '../../images/icon-2.png', text:'英国'},
-      {images: '../../images/icon-1.png', text:'罗斯'}
+      {images: '../../images/icon-1.png', text:'语文'},
+      {images: '../../images/icon-2.png', text:'数学'},
+      {images: '../../images/icon-3.png', text:'奥数'},
+      {images: '../../images/icon-4.png', text:'英语'},
+      {images: '../../images/icon-5.png', text:'物理'},
+      {images: '../../images/icon-6.png', text:'小语种'},
+      {images: '../../images/icon-7.png', text:'化学'},
+      {images: '../../images/icon-8.png', text:'生物'},
+      {images: '../../images/icon-9.png', text:'历史'},
+      {images: '../../images/icon-10.png', text:'地理'},
+      {images: '../../images/icon-11.png', text:'政治'},
+      {images: '../../images/icon-12.png', text:'音美体'}
     ]
   },
   
