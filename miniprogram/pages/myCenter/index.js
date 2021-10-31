9101929665058082643d0909571dda2f3201ddd5
luowei <luowei_jr@163.com>
2021-10-31 10:24:56 +0800

    1、个人中心页面增加用户是否授权标识 2、授权按钮增加授权页面触发功能

--- miniprogram/pages/myCenter/index.js
+++ miniprogram/pages/myCenter/index.js
@@ -22,13 +22,41 @@ Page({
         {icon: '../../images/icon-21.png',text:'教员登录'}
       ]}
     ],
+    isAuth: false
+  },
+
+  getUserProfile(e) {
+    // 推荐使用wx.getUserProfile获取用户信息，开发者每次通过该接口获取用户个人信息均需用户确认
+    // 开发者妥善保管用户快速填写的头像昵称，避免重复弹窗
+    wx.getUserProfile({
+      desc: '用于小程序中展示头像、昵称', // 声明获取用户个人信息后的用途，后续会展示在弹窗中，请谨慎填写
+      success: (res) => {
+        // 拿openId，然后把数据库中对应openI的的授权置位true
+        console.log(JSON.stringify(res))
+      },
+      fail: (res) => {
+        // 用户不同意触发
+      }
+    })
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-
+    wx.cloud.callFunction({
+      name: 'quickstartFunctions',
+      data: {
+        type: 'user',
+        func: "getUserAuthStatus"
+      }
+    }).then((resp) => {
+      this.setData({
+        isAuth: resp.result.res
+      })
+    }).catch((e) => {
+      console.log(e)
+    })
   },
 
   /**
